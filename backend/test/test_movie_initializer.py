8324053413aeb6e5636c196ac85d726d9d9b8953
Michał Ossowski <michalo2882@gmail.com>
2020-03-13 09:46:17 +0100

    add missing test for MovieInitializer._persist_movies

--- backend/test/test_movie_initializer.py
+++ backend/test/test_movie_initializer.py
@@ -1,7 +1,7 @@
 # coding: utf8
 from mock import MagicMock, call, ANY
 
-from backend import test
+from backend import test, movie
 from backend.movie_initializer import MovieInitializer
 
 
@@ -37,3 +37,14 @@ class MovieInitializerTests(test.TestCase):
         initializer._persist_movies = MagicMock()
         initializer.download_movies_data()
         self.omdb_service.fetch_movies.assert_has_calls([call(1, ANY, ANY), call(2, ANY, ANY)])
+
+    def test_persist_movies(self):
+        initializer = MovieInitializer(self.omdb_service, 100)
+        initializer._persist_movies([
+            dict(title="Dog", year=2000, imdb_id="xyz", type="movie", poster_url="http://poster"),
+            dict(title="Cat", year=2000, imdb_id="abc", type="movie", poster_url="http://poster"),
+            dict(title="Fish", year=2000, imdb_id="abc", type="movie", poster_url="http://poster"),
+        ])
+        self.assertIsNotNone(movie.Movie.get_by_title("Dog"))
+        self.assertIsNotNone(movie.Movie.get_by_title("Cat"))
+        self.assertIsNone(movie.Movie.get_by_title("Fish"))
